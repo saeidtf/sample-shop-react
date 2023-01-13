ec35fcc58b9200504544d76ce97d2e289f86f842
Saeid Taheri Fard <saeidtf@gmail.com>
2023-01-13 13:53:45 +0330

    change skelton height

--- src/pages/Products/components/ProductsSkelton.tsx
+++ src/pages/Products/components/ProductsSkelton.tsx
@@ -3,9 +3,9 @@ import { Grid, Skeleton, Stack } from "@mui/material";
 export default function ProductsSkelton() {
   return (
     <Grid container spacing={2}>
-      {Array.from({ length: 12 }).map((_, index) => (
+      {Array.from({ length: 6 }).map((_, index) => (
         <Grid item xs={12} sm={6} md={3} lg={4} key={index}>
-          <Skeleton variant="rectangular" width="100%" height={150} />
+          <Skeleton variant="rectangular" width="100%" height={400} />
           <Skeleton variant="text" width="100%" height={50}/>
           <Stack direction="row" justifyContent="space-between">
             <Skeleton variant="text" width="20%" />
