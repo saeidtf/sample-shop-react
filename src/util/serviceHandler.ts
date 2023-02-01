27617d34d3b38fce4a2673b0109c557ec34bf6d4
Saeid Taheri Fard <saeidtf@gmail.com>
2023-02-01 10:41:22 +0330

    clear axios cache

--- src/util/serviceHandler.ts
+++ src/util/serviceHandler.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { setupCache } from "axios-cache-interceptor";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const token = localStorage.getItem("token");
@@ -37,34 +36,6 @@ instance.interceptors.response.use(
   }
 );
 
-setupCache(instance,{
-    ttl: 60 * 1000,
-});
-
-instance.interceptors.request.use(
-  (config) => {
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
 
 
 export default instance;
