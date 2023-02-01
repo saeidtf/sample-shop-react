e97122a314d0e2f7261cdaffd86b35f9404d63d4
Saeid Taheri Fard <saeidtf@gmail.com>
2023-02-01 10:35:58 +0330

    change cache settiong

--- src/util/serviceHandler.ts
+++ src/util/serviceHandler.ts
@@ -41,4 +41,30 @@ setupCache(instance,{
     ttl: 60 * 1000,
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401 || error.response.status === 403) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+
 export default instance;
